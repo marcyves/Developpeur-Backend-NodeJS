1b88ce6d7c2975aa127cd367171c10a11d76b713
Marc Augier <m.augier@me.com>
2024-07-30 17:42:55 +0200

    validation et enregistrement des données du formulaire

--- Fil rouge/controller/ContactController.js
+++ Fil rouge/controller/ContactController.js
@@ -1,5 +1,26 @@
+const fs = require('fs');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
 class ContactController{
 
+    constructor(){
+        this.dataFile = './data/contact.json';
+    }
+
+    async addEntry(email, message){
+
+        let contacts = [];
+        let data = await readFile(this.dataFile, 'utf-8');
+        if(data){
+            contacts = JSON.parse(data);
+        }
+        contacts.unshift({ email, message});
+
+        return await writeFile(this.dataFile, JSON.stringify(contacts));
+    }
 
 }
 
--- Fil rouge/routes/contact.js
+++ Fil rouge/routes/contact.js
@@ -1,13 +1,41 @@
 const express = require('express');
 const router = express.Router();
 
+const { check, validationResult } = require('express-validator');
+
 module.exports = (params) => {
 
     const { contactController } = params;
 
-    router.post('/', (requete, reponse) => {
-        console.log(requete);
+    router.post('/', [
+        check('email')
+        .trim()
+        .isEmail()
+        .normalizeEmail()
+        .withMessage('Un email valide est obligatoire'),
+        check('message')
+        .trim()
+        .isLength({min: 10})
+        .escape()
+        .withMessage('Un message de plus de 10 caractères est obligatoire.')
+    ],
+    async (requete, reponse) => {
+
+        const erreurs = validationResult(requete);
+
+        let messages = {};
+
+        if(!erreurs.isEmpty()){
+            messages = {
+                erreurs: erreurs.array()
+            };
+        }else{
+            const { email, message } = requete.body;
+            await contactController.addEntry(email, message);
+        };
+
         reponse.render('layouts', { pageTitle: 'Nous avons reçu votre message',
+                                    messages: messages.erreurs,
                                     page: 'contact',
                                     contact: requete.body
                                 });
